0cc529ce2d90367749a1e15e912ca5d253216ad5
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-10-21 10:02:43 +0800

    [Feature][Joshua] Added code-mirror

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,19 @@
 import Toolbar from './components/toolbar/Toolbar'
+import CodeMirror from "@uiw/react-codemirror"
+import { vscodeDark } from "@uiw/codemirror-theme-vscode"
+import { useState } from 'react'
 
 function App(): JSX.Element {
+  const [code, setCode] = useState<string>("")
 
   return (
     <>
       <Toolbar />
+      <CodeMirror
+        value={code}
+        onChange={(code) => setCode(code)}
+        theme={vscodeDark}
+      />
     </>
   )
 }
