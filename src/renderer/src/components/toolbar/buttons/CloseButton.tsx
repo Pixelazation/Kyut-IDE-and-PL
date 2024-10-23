7c15d09da078371a9d149ecefa3ea6571c41c8e6
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-10-23 12:09:20 +0800

    [Feature][Joshua] Added new and close functionality

--- src/renderer/src/components/toolbar/buttons/CloseButton.tsx
+++ src/renderer/src/components/toolbar/buttons/CloseButton.tsx
@@ -1,8 +1,17 @@
+import { useCode } from '@renderer/contexts/code.context'
 import { FaRegWindowClose } from 'react-icons/fa'
 
 function CloseButton(): JSX.Element {
+  const { setCode, setEditorOpen } = useCode()
+
+  function handleClick(): void {
+    //TODO: Prompt for confirmation
+    setEditorOpen(false)
+    setCode('')
+  }
+
   return (
-    <button className="hover:text-pink-dark" title="Close">
+    <button className="hover:text-pink-dark" title="Close" onClick={handleClick}>
       <FaRegWindowClose />
     </button>
   )
--- src/renderer/src/components/toolbar/buttons/NewButton.tsx
+++ src/renderer/src/components/toolbar/buttons/NewButton.tsx
@@ -1,8 +1,15 @@
+import { useCode } from '@renderer/contexts/code.context'
 import { VscNewFile } from 'react-icons/vsc'
 
 function NewButton(): JSX.Element {
+  const { setEditorOpen } = useCode()
+
   return (
-    <button className="hover:text-pink-dark" title="New File">
+    <button
+      className="hover:text-pink-dark"
+      title="New File"
+      onClick={(): void => setEditorOpen(true)}
+    >
       <VscNewFile />
     </button>
   )
