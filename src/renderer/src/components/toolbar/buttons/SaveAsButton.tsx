12e8b2edba572ffc944de05e857b7fc6f78c7f3c
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-10-24 10:19:22 +0800

    [Feature][Joshua] Added functionality to Save As button

--- src/renderer/src/components/toolbar/buttons/SaveAsButton.tsx
+++ src/renderer/src/components/toolbar/buttons/SaveAsButton.tsx
@@ -2,13 +2,25 @@ import { useCode } from '@renderer/contexts/code.context'
 import { MdOutlineSaveAs } from 'react-icons/md'
 
 function SaveAsButton(): JSX.Element {
-  const { editorOpen } = useCode()
+  const { code, editorOpen, setFile } = useCode()
+
+  async function handleClick(): Promise<void> {
+    const newFile = window.api.getSaveFile()
+      
+    newFile.then((newFilePath: string) => {
+      if (newFilePath !== '') {
+        setFile(newFilePath)
+        window.api.saveFile(newFilePath, code)
+      }
+    })
+  }
 
   return (
     <button
       className="hover:text-pink-dark disabled:text-gray-600"
       title="Save As"
       disabled={!editorOpen}
+      onClick={handleClick}
     >
       <MdOutlineSaveAs />
     </button>
