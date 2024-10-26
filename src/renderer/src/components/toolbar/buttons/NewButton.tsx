6f3251437d7e7a182367c11d3d9297aefc5a4226
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-10-26 15:54:21 +0800

    [Feature][Joshua] Added prompts to New and Open when an unsaved file is open

--- src/renderer/src/components/toolbar/buttons/NewButton.tsx
+++ src/renderer/src/components/toolbar/buttons/NewButton.tsx
@@ -1,12 +1,26 @@
+import { ConfirmationOptions } from '@renderer/constants'
 import { useCode } from '@renderer/contexts/code.context'
 import { VscNewFile } from 'react-icons/vsc'
 
 function NewButton(): JSX.Element {
-  const { setEditorOpen, setFile } = useCode()
+  const { code, file, lastSavedCode, save, saveAs, setCode, setEditorOpen, setFile, setLastSavedCode } = useCode()
 
-  function handleClick() {
+  async function handleClick() {
+    if (code !== lastSavedCode) {
+      const promptResult = await window.api.confirmUnsaved()
+
+      if (promptResult === ConfirmationOptions.OK) {
+        setEditorOpen(false)
+        setCode('')
+      } else if (promptResult === ConfirmationOptions.SAVE) {
+        file === '' ? saveAs() : save()
+      }
+    }
+
     setEditorOpen(true)
     setFile('')
+    setCode('')
+    setLastSavedCode('')
   }
 
   return (
