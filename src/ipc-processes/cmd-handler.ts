f85cc3655beaad23f107cd9accd00caac8ea902e
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-11-25 09:34:18 +0800

    [Feature][Joshua] Added temporary dummy compile/run functionality

---
+++ src/ipc-processes/cmd-handler.ts
@@ -0,0 +1,39 @@
+import { exec } from 'child_process'
+
+const COMPILE_CMD = 'start cmd.exe /k echo '
+const RUN_CMD = 'start cmd.exe /k java -jar mars.jar nc'
+
+export async function handleCompile(filePath: string): Promise<void> {
+  const FULL_COMPILE_CMD = COMPILE_CMD + `"${filePath}"`
+
+  return new Promise((resolve, reject) => {
+    exec(FULL_COMPILE_CMD, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${error.message}`)
+      } else if (stderr) {
+        reject(`Stderr: ${stderr}`)
+      } else {
+        console.log(stdout)
+        resolve()
+      }
+    })
+  })
+}
+
+export async function handleRun(filePath: string): Promise<void> {
+  const FULL_RUN_CMD = RUN_CMD + ' "test.asm"'
+  console.log(filePath)
+
+  return new Promise((resolve, reject) => {
+    exec(FULL_RUN_CMD, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${error.message}`)
+      } else if (stderr) {
+        reject(`Stderr: ${stderr}`)
+      } else {
+        console.log(stdout)
+        resolve()
+      }
+    })
+  })
+}
